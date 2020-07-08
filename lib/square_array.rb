c660ba11528c74ae08b785fd34b4d397f4e00b91
John Easter <john@owltestprep.com>
2020-07-08 02:22:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,7 @@ def square_array(array = [26, 27, 28, 29, 30, 31, 32, 33])
   square_array = []
   while counter < array.length do
     square_array.push(array[counter] ** 2)
-    counter += 
-    puts square_array
+    counter += 1
   end
   square_array
 end
